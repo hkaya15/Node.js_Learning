ea557e0e5e0a6d50f3d1b66332b5f80cc920538d
hkaya15 <kayahuseyin15@gmail.com>
2023-08-18 17:59:40 +0300

    Use Template-Engine (Handlebars)

--- Express/app.js
+++ Express/app.js
@@ -4,10 +4,14 @@ const oneMid = require('./Routes/one')
 const rootDir = require('../Express/Util/path')
 const app = express();
 
+const expressHbs=require("express-handlebars");
+app.engine('handlebars',expressHbs({layoutsDir: 'Views/Layouts/',defaultLayout: 'main-layout',extname:'handlebars'})); // Template Engine settings for Handlebars
+app.set('view engine','handlebars') 
+
 const name="Huseyin";
 const products=[{name:"Apple", price:12},{name:"Orange", price:25}]
 
-app.set('view engine', 'pug'); // Template Engine settings for pug
+// app.set('view engine', 'pug'); // Template Engine settings for pug
 app.set('views', 'Views');
 
 app.use(express.static(path.join(__dirname, 'public'))); // External CSS Import
@@ -15,7 +19,7 @@ app.use(express.static(path.join(__dirname, 'public'))); // External CSS Import
 app.use('/admin', oneMid);
 
 app.get('/', (req, res, next) => {
-    res.render('home',{name,title:'Home',products}); // you can add path paramaters to manage anchor active class names
+    res.render('home',{name,title:'Home',products,hasProducts: products.length>0}); // you can add path paramaters to manage anchor active class names
     // res.sendFile(path.join(rootDir,'Views','home.html')); // If your file is on another place like dirname siblings etc. you have to add after "__dirname", "../" sign.
     // path.join detects operating system and concatenate paths. Linux => / , Windows \ 
 });
